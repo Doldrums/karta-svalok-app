35c11009ed2b6e4afb69580c59edc55b3c2c3eba
daniily000 <daniily000@yandex.ru>
2019-01-14 23:34:58 +0200

    Initial commit

---
+++ app/src/main/java/com/teamtwothree/kartasvalokapp/FormActivity.kt
@@ -0,0 +1,12 @@
+package com.teamtwothree.kartasvalokapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
